79088dc5c21330f6e7104f969754cff2b241f49c
SubashD22 <101434737+SubashD22@users.noreply.github.com>
2023-02-11 04:57:41 +0530

    set headers

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type,Authorization, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+  });
 app.get('/',(req,res)=>{
     res.send('hello')
 })
